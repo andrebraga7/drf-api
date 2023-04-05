fcb4a867fb06749f0bdc8aae3e3f48ec7e50e8d4
andrebraga7 <andrestrevisan@gmail.com>
2023-04-05 14:25:20 +0000

    Added the create post view

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,7 +6,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(surce='owner.profile.id')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
@@ -25,7 +25,7 @@ class PostSerializer(serializers.ModelSerializer):
         return value
 
     def get_is_owner(self, obj):
-        request = self.contect['request']
+        request = self.context['request']
         return request.user == obj.owner
 
     class Meta:
